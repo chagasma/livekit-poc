a2369245bf56284bbb2f63a46c990fa9d68701f1
chagasma <chagasma.dev@gmail.com>
2025-09-17 18:36:26 -0300

    feat: add both realtime and stt-llm-tts methods

--- src/main.py
+++ src/main.py
@@ -2,28 +2,44 @@ from dotenv import load_dotenv
 from livekit import agents
 from livekit.agents import AgentSession, Agent, RoomInputOptions
 from livekit.plugins import (
+    openai,
     google,
+    deepgram,
     noise_cancellation,
     silero,
 )
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
 load_dotenv(".env")
 
 
 class Assistant(Agent):
     def __init__(self) -> None:
-        super().__init__(instructions="You are a helpful voice AI assistant.")
+        super().__init__(instructions="Você é uma assistente de IA prestativa.")
 
 
 async def entrypoint(ctx: agents.JobContext):
-    session = AgentSession(
-        llm=google.beta.realtime.RealtimeModel(
-            model="gemini-2.5-flash-preview-native-audio-dialog",
-            voice="Zephyr",
-            instructions="Você é uma assistente de IA prestativa."
-        ),
-        vad=silero.VAD.load(),
-    )
+    if 1 > -1:
+        session = AgentSession(
+            llm=google.beta.realtime.RealtimeModel(
+                model="gemini-2.5-flash-preview-native-audio-dialog",
+                voice="Zephyr",
+                instructions="Você é uma assistente de IA prestativa."
+            ),
+            vad=silero.VAD.load(),
+        )
+    else:
+        session = AgentSession(
+            stt=deepgram.STT(model="nova-3", language="multi"),
+            llm=openai.LLM(model="gpt-4o-mini"),
+            tts=google.beta.GeminiTTS(
+                model="gemini-2.5-flash-preview-tts",
+                voice_name="Zephyr",
+                instructions="Você é uma assistente de IA prestativa.",
+            ),
+            vad=silero.VAD.load(),
+            turn_detection=MultilingualModel()
+        )
 
     await session.start(
         room=ctx.room,
